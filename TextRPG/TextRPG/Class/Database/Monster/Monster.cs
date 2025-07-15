69de0f4ce8c2de7dd036b3b94f0734e1579e6e5f
jyj8943 <jyj8943@gmail.com>
2025-07-15 20:22:20 +0900

    Monster.cs 수정

--- TextRPG/TextRPG/Class/Database/Monster/Monster.cs
+++ TextRPG/TextRPG/Class/Database/Monster/Monster.cs
@@ -3,16 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using TextRPG.Class.Data;
 
 namespace TextRPG.Class.Database.Monster
 {
     internal class Monster : DefaultCharacter
     {
+
         public Monster
-            (int lv, string name, string job, int str, int plusStr, int armorPoint, int plusArmorPoint, int maxHp, int plusHp, int gold, int criticalRate, int dodgeRate)
+            (int lv, string name, string job, int str, int plusStr, int armorPoint, int plusArmorPoint, int maxHp, int Hp, int gold, int criticalRate, int dodgeRate)
         {
-            
+            _lv = lv;
+            _name = name;
+            _job = job;
+            _str = str;
+            this.plusStr = plusStr;
+            _armorPoint = armorPoint;
+            this.plusArmorPoint = plusArmorPoint;
+            _maxHp = maxHp;
+            _hp = Hp;
+            _gold = gold;
+            _criticalRate = criticalRate;
+            _dodgeRate = dodgeRate;
+        }
+        public Monster(Monster other)
+        {
+            _lv = other.Lv;
+            _name = other.Name;
+            _job = other.Job;
+            _str = other.Str;
+            this.plusStr = other.plusStr;
+            _armorPoint = other.ArmorPoint;
+            this.plusArmorPoint = other.plusArmorPoint;
+            _maxHp = other.MaxHp;
+            _hp = other.Hp;
+            _plusHp = other._plusHp;
+            _gold = other.Gold;
+            _criticalRate = other.CriticalRate;
+            _dodgeRate = other.DodgeRate;
         }
     }
 }
