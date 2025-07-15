e350a52a5a660178a680440911a561b077bef1c9
KangIngu <rielkang99@gmail.com>
2025-07-15 11:21:54 +0900

    1121

--- TextRPG/TextRPG/Class/Default/DefaultItem.cs
+++ TextRPG/TextRPG/Class/Default/DefaultItem.cs
@@ -18,23 +18,23 @@ namespace TextRPG.Class.Data
        
         
 
-        protected DefaultItem(int _id, string _name, string _description, int _plusStr, int _plusArmorPoint,  int _gold)
+        protected DefaultItem(int id, string name, string description, int plusStr, int plusArmorPoint,  int gold)
         {
-            Id = _id;
-            Name = _name;
-            Description = _description;
-            PlusStr = _plusStr;
-            PlusArmorPoint = _plusArmorPoint;
+            Id = id;
+            Name = name;
+            Description = description;
+            PlusStr = plusStr;
+            PlusArmorPoint = plusArmorPoint;
             
-            Gold = _gold;
+            Gold = gold;
 
             
         }
         public class Item : DefaultItem
         {
 
-            public Item(int _id, string _name, string _description, int _plusStr, int _plusArmorPoint, int _gold)
-                : base(_id, _name, _description, _plusStr, _plusArmorPoint, _gold)
+            public Item(int id, string name, string description, int plusStr, int plusArmorPoint, int gold)
+                : base(id, name, description, plusStr, plusArmorPoint, gold)
             {
 
             }
