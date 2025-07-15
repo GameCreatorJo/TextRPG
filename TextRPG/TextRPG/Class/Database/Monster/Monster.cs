f5818dd827e6b273550c6f91164684d7b5e5b19b
zjaxjrhkd <zjaxjrhkd100@gmail.com>
2025-07-15 20:00:16 +0900

    [enhance] BattleManager 수정 #14

--- TextRPG/TextRPG/Class/Database/Monster/Monster.cs
+++ TextRPG/TextRPG/Class/Database/Monster/Monster.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using TextRPG.Class.Data;
 
 namespace TextRPG.Class.Database.Monster
@@ -22,6 +23,20 @@ namespace TextRPG.Class.Database.Monster
             _hp = hp;
             _plusHp = plusHp;
             _gold = gold;
+        }
+        public Monster(Monster other)
+        {
+            _lv = other.Lv;
+            _name = other.Name;
+            _job = other.Job;
+            _str = other.Str;
+            this.plusStr = other.plusStr;
+            _armorPoint = other.ArmorPoint;
+            this.plusArmorPoint = other.plusArmorPoint;
+            _maxHp = other.MaxHp;
+            _hp = other.Hp;
+            _plusHp = other._plusHp;
+            _gold = other.Gold;
         }
     }
 }
